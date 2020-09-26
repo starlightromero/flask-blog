bddb1091293a143ac100aa4f2727790b1ce449f7
Starlight Romero <starlightromero@gmail.com>
2020-09-26 00:05:00 -0700

    initialized flask_login

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -10,5 +11,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_blog import routes
