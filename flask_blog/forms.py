e7996da9e35248e9962424cd6ecbdea594371488
Starlight Romero <starlightromero@gmail.com>
2020-09-26 01:52:20 -0700

    added UpdateAccountForm

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,4 +1,6 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import (
     DataRequired,
@@ -55,3 +57,38 @@ class LoginForm(FlaskForm):
     )
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+
+class UpdateAccountForm(FlaskForm):
+    """Update User Account Form."""
+
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
+    )
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        """Validate username is not taken."""
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    "That username is taken. Please choose a different username."
+                )
+
+    def validate_email(self, email):
+        """Validate email is not in use."""
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    """
+                    The email is already in use. Please enter a different email.
+                    Alternatively, if you forgot your password, go to the login
+                    page and click \"Forgot Password\".
+                    """
+                )
