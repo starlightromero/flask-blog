ce4788adfc8e53a9a88325b923a335b3cad9a6b0
Starlight Romero <starlightromero@gmail.com>
2020-09-26 00:02:20 -0700

    added username and email registration form validation for db

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import (
+    DataRequired,
+    Length,
+    Email,
+    EqualTo,
+    ValidationError,
+)
+from flask_blog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -17,6 +24,28 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Sign Up")
+
+    @staticmethod
+    def validate_username(username):
+        """Validate username is not taken."""
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "That username is taken. Please choose a different username."
+            )
+
+    @staticmethod
+    def validate_email(email):
+        """Validate email is not in use."""
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                """
+                The email is already in use. Please enter a different email.
+                Alternatively, if you forgot your password, go to the login
+                page and click \"Forgot Password\".
+                """
+            )
 
 
 class LoginForm(FlaskForm):
