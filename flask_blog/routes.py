98367992821b862d7e3bf822316ff620d2364886
Starlight Romero <starlightromero@gmail.com>
2020-09-26 23:33:10 -0700

    added routes for updating and deleting posts

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_blog import app, db, bcrypt
 from flask_blog.models import User, Post
@@ -130,7 +130,41 @@ def new_post():
     return render_template("create_post.html", title="New Post", form=form)
 
 
-@app.route("/post/<int:post_id>", methods=["GET", "POST"])
-def post(post_id):
+@app.route("/post/<int:post_id>")
+def post_detail(post_id):
+    """Show details for a single post."""
     post = Post.query.get_or_404(post_id)
     return render_template("post.html", title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    """Show update post page."""
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!")
+        return redirect(url_for("post", post_id=post_id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title="Update Post", form=form)
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    """Route for deleting posts."""
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!")
+    return redirect(url_for("home"))
