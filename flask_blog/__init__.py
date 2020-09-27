0141989f47ec595132095452b4ce517a7a002337
Starlight Romero <starlightromero@gmail.com>
2020-09-26 22:55:09 -0700

    added config variables for testing

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -6,8 +6,12 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+app.config["ENV"] = "development"
+app.config["DEBUG"] = True
+app.config["TESTING"] = True
 app.config["SECRET_KEY"] = "4721760cf3e19483f9fec4b7ead533d0"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
